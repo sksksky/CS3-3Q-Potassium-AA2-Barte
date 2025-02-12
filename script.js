b2a91f61f279e83947743acacb0ae63d8a4b4c73
sksksky <spbarte22@src.pshs.edu.ph>
2025-02-12 10:50:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<button onclick="growMonsters()"></button>
+function growPopulation(){
+  const E=2.71;
+  let initialPopu = parseFloat(prompt("Enter the initial population:"));
+  let growthRate = parseFloat(prompt("Enter the rate of growth:"));
+  let time = parseFloat(prompt("Enter the time (in hours):"));
+  let finalPopu = Math.round(
+}
