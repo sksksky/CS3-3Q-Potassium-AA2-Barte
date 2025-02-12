63f6d874a117709e717676b0002737861e0d44c2
sksksky <spbarte22@src.pshs.edu.ph>
2025-02-12 11:10:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function growMonsters(){
-  const E=2.71;
   let initialPopu = parseFloat(prompt("Enter the initial population:"));
   let growthRate = parseFloat(prompt("Enter the rate of growth:"));
   let time = parseFloat(prompt("Enter the time (in hours):"));
-  let finalPopu = Math.round(Math.pow((initialPopu * E),(growthRate * time)))
+  let finalPopu = Math.round(Math.pow((initialPopu * Math.E),(growthRate * time)))
+
+  let monsterRegion = prompt("Enter the monster's region:");
+  let monsterName = prompt("Enter the monster's name:");
 }
